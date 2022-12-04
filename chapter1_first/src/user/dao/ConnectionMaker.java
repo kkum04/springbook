25cc691ffd12e2992e18603bc04b298560dede2c
KenPark <kkum04@gmail.com>
2022-12-04 21:25:15 +0900

    첫번째 코드 추가

--- chapter1_first/src/user/dao/ConnectionMaker.java
+++
@@ -1,8 +0,0 @@
-package user.dao;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
-public interface ConnectionMaker {
-    Connection makeConnection() throws ClassNotFoundException, SQLException;
-}
