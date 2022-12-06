5a8a1d5adb81bea4523cd33e524ced0fd34476d9
KenPark <kkum04@gmail.com>
2022-12-07 00:07:16 +0900

    DaoFactory에 spring 적용

--- src/main/java/springbook/user/dao/DaoFactory.java
+++ src/main/java/springbook/user/dao/DaoFactory.java
@@ -1,8 +1,17 @@
 package springbook.user.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class DaoFactory {
+  @Bean
   public UserDao userDao() {
-    ConnectionMaker connectionMaker = new DConnectionMaker();
-    return new UserDao(connectionMaker);
+    return new UserDao(connectionMaker());
+  }
+
+  @Bean
+  public ConnectionMaker connectionMaker() {
+    return new DConnectionMaker();
   }
 }
