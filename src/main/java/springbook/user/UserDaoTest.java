c88b8983f5b0130b5c4019ed205574c2bbf56926
KenPark <kkum04@gmail.com>
2022-12-09 23:49:30 +0900

    xmlfㄹㅗ 빈 설정

--- src/main/java/springbook/user/UserDaoTest.java
+++ src/main/java/springbook/user/UserDaoTest.java
@@ -2,6 +2,7 @@ package springbook.user;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 import springbook.user.dao.DaoFactory;
 import springbook.user.dao.UserDao;
 import springbook.user.domain.User;
@@ -10,7 +11,7 @@ import java.sql.SQLException;
 
 public class UserDaoTest {
   public static void main(String[] args) throws ClassNotFoundException, SQLException {
-    ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+    ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
 
     UserDao dao = context.getBean("userDao", UserDao.class);
 
