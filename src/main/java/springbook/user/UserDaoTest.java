60b55a06eb7adf325cf4cea9bf71c440143e4de8
KenPark <kkum04@gmail.com>
2022-12-09 23:58:37 +0900

    db 연결에 datasource 사용하도록 수정

--- src/main/java/springbook/user/UserDaoTest.java
+++ src/main/java/springbook/user/UserDaoTest.java
@@ -2,7 +2,6 @@ package springbook.user;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.GenericXmlApplicationContext;
 import springbook.user.dao.DaoFactory;
 import springbook.user.dao.UserDao;
 import springbook.user.domain.User;
@@ -11,7 +10,7 @@ import java.sql.SQLException;
 
 public class UserDaoTest {
   public static void main(String[] args) throws ClassNotFoundException, SQLException {
-    ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+    ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
 
     UserDao dao = context.getBean("userDao", UserDao.class);
 
