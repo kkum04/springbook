ff533a0201bba9e5422eebe61f0bfcf8aceb1e82
KenPark <kkum04@gmail.com>
2022-12-29 20:54:08 +0900

    JdbcContext 분리

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -8,26 +8,31 @@ import java.sql.*;
 
 public class UserDao {
   private DataSource dataSource;
+  private JdbcContext jdbcContext;
 
   public void setDataSource(DataSource dataSource) {
     this.dataSource = dataSource;
   }
 
+  public void setJdbcContext(JdbcContext jdbcContext) {
+    this.jdbcContext = jdbcContext;
+  }
+
   public void add(User user) throws SQLException {
-    StatementStrategy st = new StatementStrategy() {
-      @Override
-      public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
-        PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?, ?, ?)");
+    this.jdbcContext.workWithStatementStrategy(
+        new StatementStrategy() {
+          @Override
+          public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+            PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?, ?, ?)");
 
-        ps.setString(1, user.getId());
-        ps.setString(2, user.getName());
-        ps.setString(3, user.getPassword());
+            ps.setString(1, user.getId());
+            ps.setString(2, user.getName());
+            ps.setString(3, user.getPassword());
 
-        return ps;
-      }
-    };
-
-    jdbcContextStatementStrategy(st);
+            return ps;
+          }
+        }
+    );
   }
 
   public User get(String id) throws SQLException {
@@ -57,14 +62,15 @@ public class UserDao {
   }
 
   public void deleteAll() throws SQLException {
-    StatementStrategy st = new StatementStrategy() {
-      @Override
-      public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
-        PreparedStatement ps = c.prepareStatement("delete from users");
-        return ps;
-      }
-    };
-    jdbcContextStatementStrategy(st);
+    jdbcContext.workWithStatementStrategy(
+        new StatementStrategy() {
+          @Override
+          public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+            PreparedStatement ps = c.prepareStatement("delete from users");
+            return ps;
+          }
+        }
+    );
   }
 
   public int getCount() throws SQLException {
@@ -80,23 +86,5 @@ public class UserDao {
     c.close();
 
     return count;
-  }
-
-  public void jdbcContextStatementStrategy(StatementStrategy stmt) throws SQLException {
-    Connection c = null;
-    PreparedStatement ps = null;
-
-    try {
-      c = dataSource.getConnection();
-
-      ps = stmt.makePreparedStatement(c);
-
-      ps.executeUpdate();
-    } catch (SQLException e) {
-      throw e;
-    } finally {
-      if (ps != null) { try { ps.close(); } catch (SQLException e) {} }
-      if (c != null) { try { c.close(); } catch (SQLException e) {} }
-    }
   }
 }
