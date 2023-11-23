4d54adf654c0069a683dbf6b897edc1c4a6bf17d
KenPark <kkum04@gmail.com>
2023-11-23 19:20:45 +0900

    RowMapper 중복코드 제거

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -1,26 +1,29 @@
 package springbook.user.dao;
 
-import org.springframework.dao.DataAccessException;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementCreator;
-import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.RowMapper;
 import springbook.user.domain.User;
 
 import javax.sql.DataSource;
 import java.sql.*;
-import java.util.ArrayList;
 import java.util.List;
 
 public class UserDao {
-    private DataSource dataSource;
     private JdbcTemplate jdbcTemplate;
+
+    private RowMapper<User> userMapper = new RowMapper<User>() {
+        @Override
+        public User mapRow(ResultSet resultSet, int i) throws SQLException {
+            User user = new User();
+            user.setId(resultSet.getString("id"));
+            user.setName(resultSet.getString("name"));
+            user.setPassword(resultSet.getString("password"));
+            return user;
+        }
+    };
 
     public void setDataSource(DataSource dataSource) {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
-
-        this.dataSource = dataSource;
     }
 
     public void add(final User user) {
@@ -37,32 +40,14 @@ public class UserDao {
         return this.jdbcTemplate.queryForObject(
             "select * from users where id = ?",
             new Object[]{id},
-            new RowMapper<User>() {
-                @Override
-                public User mapRow(ResultSet resultSet, int i) throws SQLException {
-                    User user = new User();
-                    user.setId(resultSet.getString("id"));
-                    user.setName(resultSet.getString("name"));
-                    user.setPassword(resultSet.getString("password"));
-                    return user;
-                }
-            }
+            userMapper
         );
     }
 
     public List<User> getAll() {
         return this.jdbcTemplate.query(
             "select * from users",
-            new RowMapper<User>() {
-                @Override
-                public User mapRow(ResultSet resultSet, int i) throws SQLException {
-                    User user = new User();
-                    user.setId(resultSet.getString("id"));
-                    user.setName(resultSet.getString("name"));
-                    user.setPassword(resultSet.getString("password"));
-                    return user;
-                }
-            }
+            userMapper
         );
     }
 
