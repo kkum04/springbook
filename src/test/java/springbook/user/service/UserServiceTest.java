2c7293b5c7ae6754d32d7d6389cff4805459cb53
KenPark <kkum04@gmail.com>
2023-12-13 19:16:36 +0900

    changelevel 서비스 함수 및 테스트 작성

--- src/test/java/springbook/user/service/UserServiceTest.java
+++ src/test/java/springbook/user/service/UserServiceTest.java
@@ -1,11 +1,18 @@
 package springbook.user.service;
 
 import junit.framework.TestCase;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import springbook.user.dao.UserDao;
+import springbook.user.domain.Level;
+import springbook.user.domain.User;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
@@ -17,8 +24,43 @@ public class UserServiceTest extends TestCase {
     @Autowired
     UserService userService;
 
+    @Autowired
+    UserDao userDao;
+
+    List<User> users;
+
     @Test
     public void bean() {
         assertThat(this.userService, is(notNullValue()));
+    }
+
+    @Test
+    public void upgradeLevels() {
+        userDao.deleteAll();
+        for (User user : users) userDao.add(user);
+
+        userService.upgradeLevels();
+
+        checkLevel(users.get(0), Level.BASIC);
+        checkLevel(users.get(1), Level.SILVER);
+        checkLevel(users.get(2), Level.SILVER);
+        checkLevel(users.get(3), Level.GOLD);
+        checkLevel(users.get(4), Level.GOLD);
+    }
+
+    private void checkLevel(User user, Level expectedLevel) {
+        User userUpdate = userDao.get(user.getId());
+        assertThat(userUpdate.getLevel(), is(expectedLevel));
+    }
+
+    @Before
+    public void setUp() {
+        users = Arrays.asList(
+            new User("bumjin", "bumkjin", "p1", Level.BASIC, 49, 0),
+            new User("joytouch", "joytouch", "p2", Level.BASIC, 50, 0),
+            new User("erwins", "erwins", "p3", Level.SILVER, 60, 29),
+            new User("madnite1", "madnite1", "p4", Level.SILVER, 60, 30),
+            new User("green", "green", "p5", Level.GOLD, 100, 100)
+        );
     }
 }
