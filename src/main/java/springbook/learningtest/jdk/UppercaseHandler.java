ec6090e592fed0af01b620cd531c70fed673fe1f
KenPark <kkum04@gmail.com>
2024-01-02 18:30:30 +0900

    dynamic proxy 적용ˆ

---
+++ src/main/java/springbook/learningtest/jdk/UppercaseHandler.java
@@ -0,0 +1,22 @@
+package springbook.learningtest.jdk;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+public class UppercaseHandler implements InvocationHandler {
+    Object target;
+
+    public UppercaseHandler(Object target) {
+        this.target = target;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        Object ret = method.invoke(this.target, args);
+        if (ret instanceof String) {
+            return ((String)ret).toUpperCase();
+        }
+
+        return ret;
+    }
+}
