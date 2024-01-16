662bcce7469a4680d87f145a78c90f1e6506ac4b
KenPark <kkum04@gmail.com>
2024-01-16 16:53:46 +0900

    XmlSqlService.java 에서 전략패턴을 사용해 sql을 로드하는 부분과 sql을 가져오는 부분을 분리했음

--- src/main/java/springbook/user/sqlservice/JaxbXmlSqlReader.java
+++ src/main/java/springbook/user/sqlservice/JaxbXmlSqlReader.java
@@ -4,20 +4,20 @@ import springbook.user.dao.UserDao;
 import springbook.user.sqlservice.jaxb.SqlType;
 import springbook.user.sqlservice.jaxb.Sqlmap;
 
-import javax.annotation.PostConstruct;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import java.io.InputStream;
-import java.util.HashMap;
-import java.util.Map;
 
-public class XmlSqlService implements SqlService {
-    private Map<String, String> sqlMap = new HashMap<String, String>();
+public class JaxbXmlSqlReader implements SqlReader {
     private String sqlmapFile;
 
-    @PostConstruct
-    public void loadSql() {
+    public void setSqlmapFile(String sqlmapFile) {
+        this.sqlmapFile = sqlmapFile;
+    }
+
+    @Override
+    public void read(SqlRegistry sqlRegistry) {
         String contextPath = Sqlmap.class.getPackage().getName();
         try {
             JAXBContext context = JAXBContext.newInstance(contextPath);
@@ -26,23 +26,10 @@ public class XmlSqlService implements SqlService {
             Sqlmap sqlmap = (Sqlmap) unmarshaller.unmarshal(is);
 
             for(SqlType sql : sqlmap.getSql()) {
-                sqlMap.put(sql.getKey(), sql.getValue());
+                sqlRegistry.registerSql(sql.getKey(), sql.getValue());
             }
         } catch (JAXBException e) {
             throw new RuntimeException(e);
         }
-    }
-
-    @Override
-    public String getSql(String key) throws SqlRetrievalFailureException {
-        String sql = sqlMap.get(key);
-        if (sql == null)
-            throw new SqlRetrievalFailureException(key + "를 이용해서 SQL을 찾을 수 없습니다.");
-        else
-            return sql;
-    }
-
-    public void setSqlmapFile(String sqlmapFile) {
-        this.sqlmapFile = sqlmapFile;
     }
 }
