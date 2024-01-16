ecf03499574fe46827cd5f591cfeb90a94ed893d
KenPark <kkum04@gmail.com>
2024-01-16 13:59:38 +0900

    postconstract annotation 으로 빈 초기화

--- src/main/java/springbook/user/sqlservice/XmlSqlService.java
+++ src/main/java/springbook/user/sqlservice/XmlSqlService.java
@@ -4,6 +4,7 @@ import springbook.user.dao.UserDao;
 import springbook.user.sqlservice.jaxb.SqlType;
 import springbook.user.sqlservice.jaxb.Sqlmap;
 
+import javax.annotation.PostConstruct;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
@@ -13,13 +14,15 @@ import java.util.Map;
 
 public class XmlSqlService implements SqlService {
     private Map<String, String> sqlMap = new HashMap<String, String>();
+    private String sqlmapFile;
 
-    public XmlSqlService() {
+    @PostConstruct
+    public void loadSql() {
         String contextPath = Sqlmap.class.getPackage().getName();
         try {
             JAXBContext context = JAXBContext.newInstance(contextPath);
             Unmarshaller unmarshaller = context.createUnmarshaller();
-            InputStream is = UserDao.class.getResourceAsStream("sqlmap.xml");
+            InputStream is = UserDao.class.getResourceAsStream(this.sqlmapFile);
             Sqlmap sqlmap = (Sqlmap) unmarshaller.unmarshal(is);
 
             for(SqlType sql : sqlmap.getSql()) {
@@ -37,5 +40,9 @@ public class XmlSqlService implements SqlService {
             throw new SqlRetrievalFailureException(key + "를 이용해서 SQL을 찾을 수 없습니다.");
         else
             return sql;
+    }
+
+    public void setSqlmapFile(String sqlmapFile) {
+        this.sqlmapFile = sqlmapFile;
     }
 }
