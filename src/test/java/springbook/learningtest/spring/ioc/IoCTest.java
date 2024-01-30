a9eed0371f4fdc9ff963abfb6aea415cd69db4c6
KenPark <kkum04@gmail.com>
2024-01-30 19:03:15 +0900

    beanDefinition 사용

---
+++ src/test/java/springbook/learningtest/spring/ioc/IoCTest.java
@@ -0,0 +1,49 @@
+package springbook.learningtest.spring.ioc;
+
+import org.junit.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.RuntimeBeanReference;
+import org.springframework.beans.factory.support.RootBeanDefinition;
+import org.springframework.context.support.StaticApplicationContext;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNull.notNullValue;
+import static org.junit.Assert.assertThat;
+
+public class IoCTest {
+    @Test
+    public void helloClassRegisterTest() {
+        StaticApplicationContext ac = new StaticApplicationContext();
+        ac.registerSingleton("hello1", Hello.class);
+
+        BeanDefinition helloDef = new RootBeanDefinition(Hello.class);
+        helloDef.getPropertyValues().addPropertyValue("name", "Spring");
+        ac.registerBeanDefinition("hello2", helloDef);
+
+        Hello hello1 = ac.getBean("hello1", Hello.class);
+        assertThat(hello1, is(notNullValue()));
+
+        Hello hello2 = ac.getBean("hello2", Hello.class);
+        assertThat(hello2.sayHello(), is("Hello Spring"));
+
+        assertThat(ac.getBeanFactory().getBeanDefinitionCount(), is(2));
+    }
+
+    @Test
+    public void registerBeanWithDependency() {
+        StaticApplicationContext ac = new StaticApplicationContext();
+
+        ac.registerBeanDefinition("printer", new RootBeanDefinition(StringPrinter.class));
+
+        RootBeanDefinition helloDef = new RootBeanDefinition(Hello.class);
+        helloDef.getPropertyValues().addPropertyValue("name", "Spring");
+        helloDef.getPropertyValues().add("printer", new RuntimeBeanReference("printer"));
+
+        ac.registerBeanDefinition("hello", helloDef);
+
+        Hello hello = ac.getBean("hello", Hello.class);
+        hello.print();
+
+        assertThat(ac.getBean("printer").toString(), is("Hello Spring"));
+    }
+}
