7c87e590822add036c8760f7ac27d9d481582d09
KenPark <kkum04@gmail.com>
2024-02-11 10:27:43 +0900

    hierarchy context

--- src/test/java/springbook/learningtest/spring/ioc/GenericApplicationContextTest.java
+++ src/test/java/springbook/learningtest/spring/ioc/GenericApplicationContextTest.java
@@ -1,16 +1,45 @@
 package springbook.learningtest.spring.ioc;
 
 import org.junit.Test;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.util.ClassUtils;
+import org.springframework.util.StringUtils;
 import springbook.learningtest.spring.ioc.bean.Hello;
+import springbook.learningtest.spring.ioc.bean.Printer;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
 
 public class GenericApplicationContextTest {
+    private String basePath = StringUtils.cleanPath(ClassUtils.classPackageAsResourcePath(getClass())) + "/";
+
+
+    @Test
+    public void contextHierachy() {
+        ApplicationContext parent = new GenericXmlApplicationContext(basePath + "parentContext.xml");
+        GenericApplicationContext child = new GenericApplicationContext(parent);
+
+        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(child);
+        reader.loadBeanDefinitions(basePath + "childContext.xml");
+        child.refresh();
+
+        Printer printer = child.getBean("printer", Printer.class);
+        assertThat(printer, is(notNullValue()));
+
+        Hello hello = child.getBean("hello", Hello.class);
+        assertThat(hello, is(notNullValue()));
+
+        hello.print();
+        assertThat(printer.toString(), is("Hello Child"));
+    }
+
     @Test
     public void genericApplicationContext() {
-        GenericXmlApplicationContext ac = new GenericXmlApplicationContext("classpath:springbook/learningtest/spring/ioc/genericApplicationContext.xml");
+        ApplicationContext ac = new GenericXmlApplicationContext("classpath:springbook/learningtest/spring/ioc/genericApplicationContext.xml");
 
         Hello hello = ac.getBean("hello", Hello.class);
         hello.print();
